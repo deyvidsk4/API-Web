14adbff9c33df550f77e22aaaf2909e746c5d5cd
Deyvid José <47372050+deyvidsk4@users.noreply.github.com>
2020-08-25 18:37:35 -0300

    Add files via upload

---
+++ SQLQueryWebAPI.sql
@@ -0,0 +1,21 @@
+Create Database APIWebRest
+
+USE Polls
+
+Create Table Polls 
+(
+Poll_id int Identity primary key,
+    Poll_description varchar(200)not null,
+	options varchar(100), 
+    Votes_qty int,
+	Visualizacao int
+);
+
+INSERT INTO Polls( Poll_description,options,Votes_qty, visualizacao)
+VALUES('This is thess question','option1', 2, 4)
+
+
+
+
+
+
